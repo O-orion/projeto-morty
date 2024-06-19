a79abb2ceb13b368fd2b2f7f3cac75dd9a87ac0d
O-orion <lucas.lvtorres@gmail.com>
2024-06-19 18:28:43 -0300

    create page e componente menu

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+  inputWidth: string = '30px'; // Largura inicial do input
+
+  expandInput() {
+    this.inputWidth = '150px'; // Largura expandida ao passar o mouse
+  }
+
+  collapseInput() {
+    // Se o input não estiver focado, volta para a largura inicial
+    if (!document.activeElement || document.activeElement.tagName.toLowerCase() !== 'input') {
+      this.inputWidth = '30px';
+    }
+  }
+}
