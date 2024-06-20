9d4672c2e9f1e8cc96994b774b295764f905af9c
O-orion <lucas.lvtorres@gmail.com>
2024-06-20 14:54:02 -0300

    menu position fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './shared/components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
+  
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
