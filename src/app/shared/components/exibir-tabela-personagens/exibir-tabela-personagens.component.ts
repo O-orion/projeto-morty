b080f430acbcfa10ec24f1fe0b050cf0e3394ee2
O-orion <lucas.lvtorres@gmail.com>
2024-06-22 16:01:40 -0300

    finalizando componente de tabela

--- src/app/shared/components/exibir-tabela-personagens/exibir-tabela-personagens.component.ts
+++ src/app/shared/components/exibir-tabela-personagens/exibir-tabela-personagens.component.ts
@@ -1,16 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { Personagem } from '../../../core/types/listaPersonagens';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-exibir-tabela-personagens',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   templateUrl: './exibir-tabela-personagens.component.html',
   styleUrl: './exibir-tabela-personagens.component.scss'
 })
 export class ExibirTabelaPersonagensComponent {
 
   @Input() data!: Personagem[]
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
-  
+
+  ngOnInit() {
+    console.log('olá')
+    console.log(this.data)
+  }
+
+
 }
